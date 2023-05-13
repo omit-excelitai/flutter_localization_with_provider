9d610c9059d81a1b829e07e938275f15023ddefa
omit-excelitai <omit2023@excelitai.com>
2023-05-13 14:39:20 +0600

    Update

--- lib/provider/locale_provider.dart
+++ lib/provider/locale_provider.dart
@@ -1,51 +1,83 @@
+//
+// import 'package:flutter/material.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
+//
+// // class LocaleProvider with ChangeNotifier{
+// //   Locale _locale = const Locale('en');
+// //   Locale get locale => _locale;
+// //
+// //   void setLocale(Locale newLocale) {
+// //     _locale = newLocale;
+// //     notifyListeners();
+// //   }
+// // }
+//
+//
+//
+// class LocaleProvider with ChangeNotifier {
+//   Locale? _locale;
+//   SharedPreferences? _prefs;
+//
+//   LocaleProvider() {
+//     _loadLocale();
+//   }
+//
+//   Locale? get locale => _locale;
+//
+//   Future<void> _loadLocale() async{
+//     final languageCode = _prefs?.getString('languageCode');
+//     if (languageCode == 'bn') {
+//       _locale = Locale('bn', 'BD');
+//     } else {
+//       _locale = Locale('en', 'US'); // Set English as the default locale
+//     }
+//     // final prefs = await _prefs;
+//     // final isLocale = prefs?.getString('languageCode') ?? false;
+//     // //_locale = isLocale == 'bn' ? Locale('bn') : Locale('en');
+//     //notifyListeners();
+//   }
+//
+//   Future<void> setLocale(Locale locale) async {
+//     _locale = locale;
+//     await _prefs?.setString('languageCode', locale.languageCode);
+//     notifyListeners();
+//     // final prefs = await _prefs;
+//     // final isLocale = prefs?.getString('languageCode') ?? false;
+//     // await _prefs?.setString('languageCode', locale.languageCode);
+//     // //_locale = isLocale == 'bn'? Locale('bn') : Locale('en');
+//     // print("Check Localization State----------$_locale");
+//   }
+// }
+//
+
+
 
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-// class LocaleProvider with ChangeNotifier{
-//   Locale _locale = const Locale('en');
-//   Locale get locale => _locale;
-//
-//   void setLocale(Locale newLocale) {
-//     _locale = newLocale;
-//     notifyListeners();
-//   }
-// }
-
-
-
-class LocaleProvider with ChangeNotifier {
+class LocaleProvider extends ChangeNotifier {
   Locale? _locale;
   SharedPreferences? _prefs;
 
   LocaleProvider() {
-    _loadLocale();
+    _locale = Locale('en'); // Set the default language
+    _initPreferences();
   }
 
   Locale? get locale => _locale;
 
-  Future<void> _loadLocale() async{
-    final languageCode = _prefs?.getString('languageCode');
-    if (languageCode == 'bn') {
-      _locale = Locale('bn', 'BD');
-    } else {
-      _locale = Locale('en', 'US'); // Set English as the default locale
+  void _initPreferences() async {
+    _prefs = await SharedPreferences.getInstance();
+    final languageCode = _prefs?.getString('language_code');
+    if (languageCode != null) {
+      _locale = Locale(languageCode);
+      notifyListeners();
     }
-    // final prefs = await _prefs;
-    // final isLocale = prefs?.getString('languageCode') ?? false;
-    // //_locale = isLocale == 'bn' ? Locale('bn') : Locale('en');
-    //notifyListeners();
   }
 
-  Future<void> setLocale(Locale locale) async {
-    _locale = locale;
-    await _prefs?.setString('languageCode', locale.languageCode);
+  Future<void> setLanguage(String languageCode) async {
+    _locale = Locale(languageCode);
+    await _prefs?.setString('language_code', languageCode);
     notifyListeners();
-    // final prefs = await _prefs;
-    // final isLocale = prefs?.getString('languageCode') ?? false;
-    // await _prefs?.setString('languageCode', locale.languageCode);
-    // //_locale = isLocale == 'bn'? Locale('bn') : Locale('en');
-    // print("Check Localization State----------$_locale");
   }
 }
-
